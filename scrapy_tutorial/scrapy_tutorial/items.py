a176f03c50d4f13d8bca06d80bfc992ba10ce062
Nguyen hung cuong <barcode.cuongnh@hblab.vn>
2020-10-23 11:17:59 +0700

    update code work with Item

--- scrapy_tutorial/scrapy_tutorial/items.py
+++ scrapy_tutorial/scrapy_tutorial/items.py
@@ -1,12 +1,57 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
 
+# PART 2: work with Item in scrapy
 import scrapy
+from scrapy.item import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
+
+# MapCompose enables us to apply multiple processing functions to a field
+# TakeFirst processor takes the first value of the list.
 
 
-class ScrapyTutorialItem(scrapy.Item):
+def remove_quotes(text):
+    # strip the unicode quotes
+    text = text.strip(u'\u201c'u'\u201d')
+    return text
+
+
+def convert_date(text):
+    # convert string March 14, 1879 to Python date
+    return datetime.strptime(text, '%B %d, %Y')
+
+
+def parse_location(text):
+    # parse location "in Ulm, Germany"
+    # this simply remove "in ", you can further parse city, state, country, etc.
+    return text[3:]
+
+
+class QuoteItem(scrapy.Item):
+
     # define the fields for your item here like:
     # name = scrapy.Field()
+
+    # pre-processing after get value text
+    quote_content = Field(
+        input_processor=MapCompose(remove_quotes),
+        # TakeFirst return the first value not the whole list
+        output_processor=TakeFirst()
+    )
+    tags = Field()
+    author_name = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    author_birthday = Field(
+        input_processor=MapCompose(convert_date),
+        output_processor=TakeFirst()
+    )
+    author_bornlocation = Field(
+        input_processor=MapCompose(parse_location),
+        output_processor=TakeFirst()
+    )
+    author_bio = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
     pass
--- scrapy_tutorial/scrapy_tutorial/spiders/quotes_spiders.py
+++ scrapy_tutorial/scrapy_tutorial/spiders/quotes_spiders.py
@@ -1,10 +1,13 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy_tutorial.items import QuoteItem
+# ItemLoader can process raw_data which get from the css (define in Item)
 
 
 class QuotesSpider(scrapy.Spider):
     # name of spider
     name = "quotes"
-
+    allowed_domains = ["toscrape.com"]
     start_urls = ["http://quotes.toscrape.com"]
 
     def parse(self, response):
@@ -13,17 +16,26 @@ class QuotesSpider(scrapy.Spider):
         self.logger.info("hello this is my first crawl")
         quotes = response.css('div.quote')
         for quote in quotes:
-            yield {
-                # get value in element with ".class_css_name::text"
-                "text": quote.css(".text::text").get(),
-                "author": quote.css(".author::text").get(),
-                "tags": quote.css(".tag::text").get(),
-            }
+            # yield {
+            #     # get value in element with ".class_css_name::text"
+            #     "text": quote.css(".text::text").get(),
+            #     "author": quote.css(".author::text").get(),
+            #     "tags": quote.css(".tag::text").get(),
+            # }
+
+            loader = ItemLoader(item=QuoteItem(), selector=quote)
+            # pay attention to the dot .// to use relative xpath
+            # loader.add_xpath('quote_content', ".//span[@class='text']/text()")
+            loader.add_css('quote_content', '.text::text')
+            # loader.add_xpath('author', './/small//text()')
+            loader.add_css('tags', '.tag::text')
+            quote_item = loader.load_item()
 
             author_url = quote.css('.author + a::attr(href)').get()
             self.logger.info('get author page url')
-            # go to the author page
-            yield response.follow(author_url, callback=self.parse_author)
+
+            # pass metadata quote_item after crawl quote_content then crawl author by other link
+            yield response.follow(author_url, callback=self.parse_author, meta={'quote_item': quote_item})
 
         # find element html to click next page
         next_page = response.css('li.next a::attr(href)').get()
@@ -35,9 +47,16 @@ class QuotesSpider(scrapy.Spider):
         pass
 
     def parse_author(self, response):
-        yield {
-            'author_name': response.css('.author-title::text').get(),
-            'author_birthday': response.css('.author-born-date::text').get(),
-            'author_bornlocation': response.css('.author-born-location::text').get(),
-            'author_bio': response.css('.author-description::text').get(),
-        }
+        quote_item = response.meta['quote_item']
+        loader = ItemLoader(item=quote_item, response=response)
+        loader.add_css('author_name', '.author-title::text')
+        loader.add_css('author_birthday', '.author-born-date::text')
+        loader.add_css('author_bornlocation', '.author-born-location::text')
+        loader.add_css('author_bio', '.author-description::text')
+        yield loader.load_item()
+        # yield {
+        #     'author_name': response.css('.author-title::text').get(),
+        #     'author_birthday': response.css('.author-born-date::text').get(),
+        #     'author_bornlocation': response.css('.author-born-location::text').get(),
+        #     'author_bio': response.css('.author-description::text').get(),
+        # }
